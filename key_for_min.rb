da13644c25286d1d8a63dc36ff302a946f885ad5
Sarah Rose <sarahrose63089@gmail.com>
2020-06-08 17:25:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,7 +13,7 @@ def key_for_min_value(name_hash) #-- how do i do this without this method?
   smallest = nil
   name_hash.each {|key, value|
     #smallest = value #always returns last value
-    if smallest == nil
+    if smallest
       smallest = value
     end
     if value <= smallest
