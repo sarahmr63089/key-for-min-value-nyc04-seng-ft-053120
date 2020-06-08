8b4f87a726141f2aefedf5574fdf09930b8fc105
Sarah Rose <sarahrose63089@gmail.com>
2020-06-08 17:27:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ require "pry"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash) 
+def key_for_min_value(name_hash)
   answer = nil
   smallest = nil
   name_hash.each {|key, value|
@@ -16,6 +16,3 @@ def key_for_min_value(name_hash)
   }
   answer
 end
-
-hash = {:potato => 45, :apple => 1, :banana => 8}
-key_for_min_value(hash)
